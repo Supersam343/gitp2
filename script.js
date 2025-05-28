a601e73040a80c63efcfad123feba1a6373a6f89
Supersam343 <Supersam343@gmail.com>
2025-05-28 13:06:27 -0700

    finished the game portion ready for clean up and adjustments

--- script.js
+++ script.js
@@ -41,30 +41,17 @@ class Deck{
 // shuffles the deck by starting at the end of the array and moving backwards, then it picks a random index from  
 // "o" then swapping them with the the value at the current i index.
 
- shuffleDeck() {
-    for(let i = this.deck.length - 1; i > 0; i--) {
-        let o = Math.floor(Math.random()* (i + 1));
-
-        [this.deck[i], this.deck[o]] = [this.deck[o], this.deck[i]];
-    }
-   }
-
-// gives each player their respective deck   
- dealDeck(players, shuffledDeck) {
-    let hand1 = shuffledDeck.splice(0,26);
-    for (let i = 0; i < hand1.length; i++){
-        players[0].hands.push(hand1[i]);
-    }
-
-    let hand2 = shuffledDeck.splice(0,26);
-    for(let i = 0; i < hand2.length;i++){
-        players[1].hands.push(hand2[i]);
-    }
- }
+shuffleDeck() {
+  const deck = this.deck;
+  for (let i = deck.length - 1; i > 0; i--) {
+    const o = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[o]] = [deck[o], deck[i]];
+  }
+}
 
 }
 
-
+// class for the game 
 
 class Game {
     constructor() {
@@ -81,21 +68,56 @@ class Game {
     }
     
  playGame() {
+    const deck = new Deck
+    deck.createDeck()
+    deck.shuffleDeck()
 
- }
+     while(deck.deck.length !== 0) {
+
+        this.player1.hand.push(deck.deck.shift())
+        this.player2.hand.push(deck.deck.shift())
+     }
+
+     for (let i= 0; i < this.player1.hand.length; i++) {
+        if(this.player1.hand[i].value > this.player2.hand[i].value) {
+            this.player1.score ++
+            console.log(`
+            Player1 card: ${this.player1.hand[i].name}
+            Player2 card: ${this.player2.hand[i].name}
+            player one experiences a win!
+            score: 
+            Player1: ${this.player1.score} player2: ${this.player2.score}`)
+     } else if (this.player2.hand[i].value > this.player1.hand[i].value) {
+            this.player1.score ++
+            console.log(`
+            Player1 card: ${this.player1.hand[i].name}
+            Player2 card: ${this.player2.hand[i].name}
+            player two experiences a win!
+            score: 
+            Player1: ${this.player1.score} player2: ${this.player2.score}`)
+     } else {
+         console.log(`
+            Player1 card: ${this.player1.hand[i].name}
+            Player2 card: ${this.player2.hand[i].name}
+            tie no one experiences a win, womp womp womp
+            score: 
+            Player1: ${this.player1.score} player2: ${this.player2.score}`)
+      }
+    } 
+     if (this.player1.score > this.player2.score) {
+        console.log('player1 wins the chicken dinner!!!')
+     } else if (this.player2.score > this.player1.score) {
+        console.log('player2 wins the chicken dinner!!!')
+     } else {
+        console.log('wow a fluke! it was a tie!')
+     }
+  }
 
 
 }
 
 
+const game = new Game
+game.playGame()
 
-
-
-
-
-
-const deck = new Deck
-deck.createDeck()
-deck.shuffleDeck()
-deck.dealDeck()
-console.log(deck.deck)
+// console.log(deck.deck)
